498b5fc59b6c3d83ffe3748b5013f2e4b394b905
arnaskro <arnas.kromelis@gmail.com>
2015-09-23 09:05:12 +0200

    Initial Commit (Correct one)

--- app/src/main/java/ark/viauc/mobi/fourthhandin/Intro.java
+++ app/src/main/java/ark/viauc/mobi/fourthhandin/Intro.java
@@ -5,18 +5,18 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class Menu extends AppCompatActivity {
+public class Intro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_intro);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_menu, menu);
+        getMenuInflater().inflate(R.menu.menu_intro, menu);
         return true;
     }
 
