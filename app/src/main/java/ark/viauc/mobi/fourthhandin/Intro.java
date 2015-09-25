c301c7991264aa359d8b2539064d655365708c7b
arnaskro <arnas.kromelis@gmail.com>
2015-09-25 18:27:03 +0200

    Map

--- app/src/main/java/ark/viauc/mobi/fourthhandin/Intro.java
+++ app/src/main/java/ark/viauc/mobi/fourthhandin/Intro.java
@@ -1,52 +1,227 @@
 package ark.viauc.mobi.fourthhandin;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Intro extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.Marker;
 
-    Button btnQuit, btnCamera;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Intro extends AppCompatActivity implements
+        GoogleMap.OnMarkerClickListener,
+        OnMapReadyCallback,
+        View.OnClickListener {
+
+    final static String CAMERA_PREFS = "CAMERA_PREFS";
+    final static String FILENAME = "FILENAME";
+    final static int REQUEST_FILE = 100;
+    final static int RESULT_TRUE = -1;
+    final static int RESULT_FALSE = 0;
+
+    private GoogleMap gmap;
+
+    Button btnTakePicture;
+    File dirPictures;
+    ImageView ivLastImage;
+    TextView txtOrientation, txtLatitude, txtLongtitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
 
+        // Check acces to external storage
+        checkAccess();
+
+        // Setup directory
+        setDirectory();
+
         // Setup buttons
         setupButtons();
+
+        // Setup text views
+        setupTextViews();
+
+        // Setup image view for last image
+        ivLastImage = (ImageView) findViewById(R.id.ivLastImage);
+
+        // Map
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+    }
+
+    private void setupTextViews() {
+        txtOrientation = (TextView) findViewById(R.id.txtOr);
+        txtLatitude = (TextView) findViewById(R.id.txtLat);
+        txtLongtitude = (TextView) findViewById(R.id.txtLong);
+    }
+
+    private void setDirectory() {
+        dirPictures = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        if (dirPictures != null) dirPictures.mkdirs();
+    }
+
+
+    private void checkAccess() {
+        if (isExternalStorageWritable() && isExtrenalStorageReadable()) {
+            Toast.makeText(this, "External storage is accessible", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "External storage is not accessible!", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void setupButtons() {
-        // Setup quit button
-        btnQuit = (Button) findViewById(R.id.btnQuit);
-        btnQuit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-
-        // Setup camera button
-        btnCamera = (Button) findViewById(R.id.btnCamera);
-        btnCamera.setOnClickListener(new View.OnClickListener() {
+        btnTakePicture = (Button) findViewById(R.id.btnTakePicutre);
+        btnTakePicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent intentCamera = new Intent(Intro.this, Camera.class);
-                startActivity(intentCamera);
+                // Create filename
+                String timestamp = new SimpleDateFormat("yyyMMdd_HHmmss").format(new Date());
+                String filename = dirPictures.getPath() + File.separator + "IMG_" + timestamp + ".jpg";
+                File imagefile = new File(filename);
+                imagefile.delete();
+                try {
+                    imagefile.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Uri imageuri = Uri.fromFile(imagefile);
+
+                // Save last attempted image capture filename
+                saveAttemptFilename(filename);
+
+                // Take picture
+                takePicture(imageuri);
             }
         });
+    }
+
+    private void saveAttemptFilename(String filename) {
+        SharedPreferences prefs = getSharedPreferences(CAMERA_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor edit = prefs.edit();
+        edit.putString(FILENAME, filename);
+        edit.apply();
+    }
+
+    private String loadAttemptFilename() {
+        String timestamp = new SimpleDateFormat("yyyMMdd_HHmmss").format(new Date());
+        String otherFilename = dirPictures.getPath() + File.separator + "IMG_" + timestamp + ".jpg";
+
+        SharedPreferences prefs = getSharedPreferences(CAMERA_PREFS, MODE_PRIVATE);
+        return prefs.getString(FILENAME, otherFilename);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+
+        if (status== ConnectionResult.SUCCESS)
+            Toast.makeText(this, "Google Play Services Are Available", Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(this, "Google Play Services Are NOT Available!", Toast.LENGTH_LONG).show();
+
+        String filename = loadAttemptFilename();
+        setPicture(filename, ivLastImage);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_FILE && resultCode == RESULT_TRUE) {
+            String filename = loadAttemptFilename();
+
+            setPicture(filename, ivLastImage);
+        }
+
+    }
+
+    private void setPicture(String filename, ImageView ivLastImage) {
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(filename, bmOptions);
+
+        int scaleFactor = Math.min(bmOptions.outWidth / 200, bmOptions.outHeight / 200);
+
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        Bitmap bitmap = BitmapFactory.decodeFile(filename, bmOptions);
+
+        try {
+            ExifInterface exif = new ExifInterface(filename);
+
+            // Set texts
+            txtOrientation.setText("Orientation: \n\t\t" + exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, -1));
+
+            float[] latlong = new float[2];
+            if(exif.getLatLong(latlong)){
+                txtLatitude.setText("Latitude:\n\t\t"+latlong[0]);
+                txtLongtitude.setText("Longtitude: \n\t\t"+latlong[1]);
+            }
+
+        } catch (IOException e) {
+        }
+
+        ivLastImage.setRotation(90);
+        ivLastImage.setImageBitmap(bitmap);
+    }
+
+    private void takePicture(Uri imageuri) {
+        Intent intentGoToCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intentGoToCamera.putExtra(MediaStore.EXTRA_OUTPUT, imageuri);
+
+        if (intentGoToCamera.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intentGoToCamera, REQUEST_FILE);
+        }
+    }
+
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isExtrenalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) || Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_intro, menu);
+        getMenuInflater().inflate(R.menu.menu_camera, menu);
         return true;
     }
 
@@ -63,5 +238,30 @@ public class Intro extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.setOnMarkerClickListener(this);
+
+        findImagesWithGeoTagAndAddToGmap(googleMap);
+
+    }
+
+    private void findImagesWithGeoTagAndAddToGmap(GoogleMap googleMap) {
+        // TODO not implemented
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+
+        Toast.makeText(this, "ImageName: "+marker.getTitle(), Toast.LENGTH_LONG).show();;
+
+        return true;
     }
 }
